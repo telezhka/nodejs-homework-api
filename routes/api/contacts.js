3020c4ea911faec1465c95faf6b49f2c0f0d2f9e
Roman Telik <rtelik58@gmail.com>
2023-10-10 21:13:42 +0300

    maybe-done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -24,14 +24,14 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const contact = await contacts.getContactById(id);
+    const contact = await getContactById(id);
     if (contact) {
       res.status(200).json(contact);
     } else {
       res.status(404).json({ message: "Not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Errorrr" });
   }
 });
 
@@ -44,7 +44,7 @@ router.post("/", async (req, res) => {
   }
 
   try {
-    const newContact = await contacts.addContact(name, email, phone);
+    const newContact = await addContact(name, email, phone);
     res.status(201).json(newContact);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
@@ -56,7 +56,7 @@ router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const removedContact = await contacts.removeContact(id);
+    const removedContact = await removeContact(id);
     if (removedContact) {
       res.status(200).json({ message: "Contact deleted" });
     } else {
@@ -77,7 +77,7 @@ router.put("/:id", async (req, res) => {
   }
 
   try {
-    const updatedContact = await contacts.updateContact(id, {
+    const updatedContact = await updateContact(id, {
       name,
       email,
       phone,
