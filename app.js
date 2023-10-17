64e8edb32a79bfadc027fe39ae76b7dcfb0a919f
Roman Telik <rtelik58@gmail.com>
2023-10-17 17:59:01 +0300

    start

--- app.js
+++ app.js
@@ -12,7 +12,44 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+// Підключення до MongoDB
+mongoose
+  // mongodb+srv://telezhka:1PzEfDmrz6@cluster0.yobrik7.mongodb.net/?retryWrites=true&w=majority
+  .connect("mongodb://localhost:27017/contactsdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  });
+
+// Схема моделі для колекції контактів
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+app.use("/api/contacts", contactsRouter(Contact));
+
+// app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
