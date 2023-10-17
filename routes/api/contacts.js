0d1ff26831b12acfcd9c8a60b7335b08222e430b
Roman Telik <rtelik58@gmail.com>
2023-10-17 17:42:12 +0300

    project-init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const contacts = require("../../models/contacts.json");
 const {
   listContacts,
   getContactById,
   removeContact,
-  // addContact,
-  // updateContact,
+
 } = require("../../models/contacts");
 const { createContact, updateContactt } = require('../../controllers/contactsController');
 
@@ -37,20 +35,6 @@ router.get("/:id", async (req, res) => {
 });
 
 // Додати новий контакт
-// router.post("/", async (req, res) => {
-//   const { name, email, phone } = req.query;
-
-//   if (!name || !email || !phone) {
-//     return res.status(400).json({ message: "Missing required fields" });
-//   }
-
-//   try {
-//     const newContact = await addContact(name, email, phone);
-//     res.status(201).json(newContact);
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
 router.post('/', createContact);
 
 // Видалити контакт за ID
@@ -70,29 +54,6 @@ router.delete("/:id", async (req, res) => {
 });
 
 // Оновити контакт за ID
-// router.put("/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const { name, email, phone } = req.query;
-
-//   if (!name || !email || !phone) {
-//     return res.status(400).json({ message: "Missing fields" });
-//   }
-
-//   try {
-//     const updatedContact = await updateContact(id, {
-//       name,
-//       email,
-//       phone,
-//     });
-//     if (updatedContact) {
-//       res.status(200).json(updatedContact);
-//     } else {
-//       res.status(404).json({ message: "Not found" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
 router.put('/:id', updateContactt);
 
 module.exports = router;
