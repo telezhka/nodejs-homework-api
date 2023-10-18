1e2f621fc12a6682f3a896528685a58461c09888
Roman Telik <rtelik58@gmail.com>
2023-10-18 22:39:05 +0300

    this-shit-works-and-great

--- server.js
+++ server.js
@@ -1,66 +1,3 @@
-// // const app = require('./app')
-
-// // app.listen(3000, () => {
-// //   console.log("Server running. Use our API on port: 3000")
-// // })
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-
-// require("dotenv").config();
-
-// const app = express();
-
-// // parse application/json
-// app.use(express.json());
-// // cors
-// app.use(cors());
-
-// const routerApi = require("./routes/api/contacts");
-// app.use("/api", routerApi);
-
-// app.use((_, res, __) => {
-//   res.status(404).json({
-//     status: "error",
-//     code: 404,
-//     message: "Use api on routes: /api/tasks",
-//     data: "Not found",
-//   });
-// });
-
-// app.use((err, _, res, __) => {
-//   console.log(err.stack);
-//   res.status(500).json({
-//     status: "fail",
-//     code: 500,
-//     message: err.message,
-//     data: "Internal Server Error",
-//   });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// const uriDb = process.env.DB_HOST;
-
-// const connection = mongoose.connect(
-//   "mongodb+srv://telezhka:1PzEfDmrz6@cluster0.yobrik7.mongodb.net/db-contacts?retryWrites=true&w=majority",
-//   {
-//     // promiseLibrary: global.Promise,
-//     useNewUrlParser: true,
-//     // useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     // useFindAndModify: false,
-//   }
-// );
-
-// connection
-//   .then(() => {
-//     app.listen(PORT, function () {
-//       console.log(`Server running. Use our API on port: ${PORT}`);
-//     });
-//   })
-//   .catch((err) =>
-//     console.log(`Server not running. Error message: ${err.message}`)
-//   );
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -95,25 +32,8 @@ mongoose
   });
 
 // Схема моделі для колекції контактів
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
-  },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const Contact = mongoose.model("Contact", contactSchema);
-
+const Contact = require("./service/schemas/contactSchema");
+// const Contact = mongoose.model("Contact", contactSchema);
 app.use("/api/contacts", contactsRouter(Contact));
 
 app.listen(port, () => {
--- service/schemas/contactSchema.js
+++ service/schemas/contactSchema.js
@@ -1,26 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const task = new Schema(
-  {
-    title: {
-      type: String,
-      minlength: 2,
-      maxlength: 70,
-    },
-    text: {
-      type: String,
-      minlength: 3,
-      maxlength: 170,
-    },
-    isDone: {
-      type: Boolean,
-      default: false,
-    },
+// const task = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       minlength: 2,
+//       maxlength: 70,
+//     },
+//     text: {
+//       type: String,
+//       minlength: 3,
+//       maxlength: 170,
+//     },
+//     isDone: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { versionKey: false, timestamps: true }
+// );
+
+// const Task = mongoose.model("task", task);
+
+// module.exports = Task;
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
   },
-  { versionKey: false, timestamps: true }
-);
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const Task = mongoose.model("task", task);
-
-module.exports = Task;
+const Contact = mongoose.model("Contact", contactSchema);
+module.exports = Contact;
