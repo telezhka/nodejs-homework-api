97c12f00210cb2b6a80b9213aa9419d678ef89bb
Roman Telik <rtelik58@gmail.com>
2023-10-31 17:34:49 +0200

    step-one-done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const contactsRouter = require("./routes/api/contacts");
 const userCurRouter = require("./routes/api/userCur");
@@ -16,7 +17,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "public")));
 // Підключення до MongoDB
 mongoose
   .connect(
